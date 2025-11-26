e331d3843cc86342dc25390f487970a0441d8ab5
Luis Fábregas-Ibáñez <luisfabib@gmail.com>
2025-11-26 11:10:33 +0100

    feat: implement correct use of %resource and %rootResource FHIRPath environment variables

--- test/test_fhir_path_engine_environment.py
+++ test/test_fhir_path_engine_environment.py
@@ -2,6 +2,13 @@ import pytest
 
 from fhircraft.fhir.path.engine.environment import *
 from fhircraft.fhir.path.exceptions import FHIRPathError
+from fhircraft.fhir.resources.datatypes.R4 import (
+    Patient,
+    Observation,
+    Patient,
+    Reference,
+)
+
 
 # -------------
 # Environment variables
@@ -27,18 +34,30 @@ def test_env_variable_string_representation():
 
 
 def test_default_context_variable_is_set():
-    value = 42
-    assert EnvironmentVariable("%context").single(value) == value
+    reference = Reference(reference="patient-1")
+    observation = Observation(subject=reference, meta=None)
+    patient = Patient(id="patient-1", contained=[observation])  # type: ignore
+    assert EnvironmentVariable("%context").single(patient) == patient
+    assert EnvironmentVariable("%context").single(observation) == observation
+    assert EnvironmentVariable("%context").single(reference) == reference
 
 
 def test_default_resource_variable_is_set():
-    value = 42
-    assert EnvironmentVariable("%resource").single(value) == value
+    reference = Reference(reference="patient-1")
+    observation = Observation(subject=reference, meta=None)
+    patient = Patient(id="patient-1", contained=[observation])  # type: ignore
+    assert EnvironmentVariable("%resource").single(patient) == patient
+    assert EnvironmentVariable("%resource").single(observation) == observation
+    assert EnvironmentVariable("%resource").single(reference) == observation
 
 
 def test_default_root_resource_variable_is_set():
-    value = 42
-    assert EnvironmentVariable("%rootResource").single(value) == value
+    reference = Reference(reference="patient-1")
+    observation = Observation(subject=reference, meta=None)
+    patient = Patient(id="patient-1", contained=[observation])  # type: ignore
+    assert EnvironmentVariable("%rootResource").single(patient) == patient
+    assert EnvironmentVariable("%rootResource").single(observation) == patient
+    assert EnvironmentVariable("%rootResource").single(reference) == patient
 
 
 def test_default_ucum_variable_is_set():
