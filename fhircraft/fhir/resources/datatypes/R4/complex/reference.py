9a643445d212023b17c3c84c4cc3313455fd9e84
Luis Fábregas-Ibáñez <48292540+luisfabib@users.noreply.github.com>
2025-12-01 14:31:29 +0000

    fix: update FHIR reference validation to handle local references correctly

--- fhircraft/fhir/resources/datatypes/R4/complex/reference.py
+++ fhircraft/fhir/resources/datatypes/R4/complex/reference.py
@@ -94,9 +94,11 @@ class Reference(Element):
 
     @model_validator(mode="after")
     def FHIR_ref_1_constraint_model_validator(self):
+        if getattr(self, "_resource", None) == self:
+            return self
         return fhir_validators.validate_model_constraint(
             self,
-            expression="%rootResource.is(Reference) or reference.startsWith('#').not() or (reference.substring(1).trace('url') in %rootResource.contained.id.trace('ids'))",
+            expression="reference.startsWith('#').not() or (reference.substring(1).trace('url') in %rootResource.contained.id.trace('ids'))",
             human="SHALL have a contained resource if a local reference is provided",
             key="ref-1",
             severity="error",
--- fhircraft/fhir/resources/datatypes/R5/complex/reference.py
+++ fhircraft/fhir/resources/datatypes/R5/complex/reference.py
@@ -103,6 +103,8 @@ class Reference(DataType):
 
     @model_validator(mode="after")
     def FHIR_ref_2_constraint_model_validator(self):
+        if getattr(self, "_resource", None) == self:
+            return self
         return fhir_validators.validate_model_constraint(
             self,
             expression="reference.exists() or identifier.exists() or display.exists() or extension.exists()",
