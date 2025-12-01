449f922f194cd796efd48177c1237226268c18f1
Luis Fábregas-Ibáñez <48292540+luisfabib@users.noreply.github.com>
2025-12-01 10:34:32 +0100

    Fix `DomainResource` invariant `dom-3` for all FHIR releases prior to R5 (#130)

--- fhircraft/fhir/resources/datatypes/R4/core/medication_statement.py
+++ fhircraft/fhir/resources/datatypes/R4/core/medication_statement.py
@@ -289,7 +289,7 @@ class MedicationStatement(DomainResource):
     def FHIR_dom_3_constraint_model_validator(self):
         return fhir_validators.validate_model_constraint(
             self,
-            expression="contained.where((('#'+id in (%resource.descendants().reference | %resource.descendants().as(canonical) | %resource.descendants().as(uri) | %resource.descendants().as(url))) or descendants().where(reference = '#').exists() or descendants().where(as(canonical) = '#').exists() or descendants().where(as(canonical) = '#').exists()).not()).trace('unmatched', id).empty()",
+            expression="contained.where((('#'+id in (%resource.descendants().reference | %resource.descendants().ofType(canonical) | %resource.descendants().ofType(uri) | %resource.descendants().ofType(url))) or descendants().where(reference = '#').exists() or descendants().where(as(canonical) = '#').exists() or descendants().where(as(canonical) = '#').exists()).not()).trace('unmatched', id).empty()",
             human="If the resource is contained in another resource, it SHALL be referred to from elsewhere in the resource or SHALL refer to the containing resource",
             key="dom-3",
             severity="error",
