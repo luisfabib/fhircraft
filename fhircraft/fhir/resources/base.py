49d70bfad4202afc7500cf04ab2542c03cdf86bc
Luis Fábregas-Ibáñez <luisfabib@gmail.com>
2026-01-16 20:44:49 +0100

    Set extra configuration to "forbid" in FHIRBaseModel for stricter validation

--- fhircraft/fhir/resources/base.py
+++ fhircraft/fhir/resources/base.py
@@ -52,6 +52,7 @@ class FHIRBaseModel(BaseModel, FHIRPathMixin):
         defer_build=True,
         validate_by_alias=True,
         validate_by_name=True,
+        extra="forbid",
     )
     _fhir_release: ClassVar[str]
 
