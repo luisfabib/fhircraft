379777fa7839c8da85a93efd553ec4b978d6ec58
Luis Fábregas-Ibáñez <luisfabib@gmail.com>
2026-01-31 18:35:36 +0100

    add missing backbone elements for complex R5 Availability type

--- fhircraft/fhir/resources/datatypes/R5/complex/availability.py
+++ fhircraft/fhir/resources/datatypes/R5/complex/availability.py
@@ -4,7 +4,53 @@ from pydantic import Field, model_validator
 
 import fhircraft.fhir.resources.validators as fhir_validators
 from fhircraft.fhir.resources.datatypes.primitives import *
-from fhircraft.fhir.resources.datatypes.R5.complex import Element
+from fhircraft.fhir.resources.datatypes.R5.complex import (
+    Element,
+    BackboneElement,
+    Period,
+)
+
+
+class AvailabilityAvailableTime(BackboneElement):
+    """
+    Times the {item} is available
+    """
+
+    daysOfWeek: Optional[List[Code]] = Field(
+        description="Days of the week when the {item} is available",
+        default=None,
+    )
+    allDay: Optional[Boolean] = Field(
+        description="Is this always available? e.g. 24 hour service",
+        default=None,
+    )
+    availableStartTime: Optional[Time] = Field(
+        description="Opening time of day (ignored if allDay = true)",
+        default=None,
+    )
+    availableEndTime: Optional[Time] = Field(
+        description="Closing time of day (ignored if allDay = true)",
+        default=None,
+    )
+
+
+class AvailabilityNotAvailableTime(BackboneElement):
+    """
+    Not available during this time due to provided reason
+    """
+
+    description: String = Field(
+        description="Reason presented to the user explaining why time not available",
+    )
+    description_ext: Optional[Element] = Field(
+        description="Placeholder element for description extensions",
+        default=None,
+        alias="_description",
+    )
+    during: Optional[Period] = Field(
+        description="Service not available from this date",
+        default=None,
+    )
 
 
 class Availability(Element):
@@ -14,11 +60,11 @@ class Availability(Element):
 
     _type = "Availability"
 
-    availableTime: Optional[List[Element]] = Field(
+    availableTime: Optional[List[AvailabilityAvailableTime]] = Field(
         description="Times the {item} is available",
         default=None,
     )
-    notAvailableTime: Optional[List[Element]] = Field(
+    notAvailableTime: Optional[List[AvailabilityNotAvailableTime]] = Field(
         description="Not available during this time due to provided reason",
         default=None,
     )
