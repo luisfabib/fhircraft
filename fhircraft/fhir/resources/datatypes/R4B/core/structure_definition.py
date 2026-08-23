cf4289d3ed1cb1c7bf06355b7652abe75e0fcb11
botbikamordehai2-sketch <botbikamordehai2@gmail.com>
2026-08-23 09:37:21 +0000

    fix: Preserve Pydantic field constraints in FHIR model generation (closes #406)

--- fhircraft/fhir/resources/datatypes/R4B/core/structure_definition.py
+++ fhircraft/fhir/resources/datatypes/R4B/core/structure_definition.py
@@ -69,6 +69,7 @@ class StructureDefinitionSnapshot(BackboneElement):
     element: Optional[ListType[ElementDefinition]] = Field(
         description="Definition of elements in the resource (if no StructureDefinition)",
         default=None,
+        min_length=1,
     )
 
 
